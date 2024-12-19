7ab20ef070d81cc94d1807d66c7bd96f8047934b
helix <ayushrajmth91@gmail.com>
2024-12-19 12:32:30 +0530

    NVIM Improvements

--- Dynamic Colors/nvim/lua/plugins/mini.lua
+++ Dynamic Colors/nvim/lua/plugins/mini.lua
@@ -1,37 +1,11 @@
 return {
-  {
-    'echasnovski/mini.ai',
-    version = false,
-    config = function()
-      require('mini.ai').setup()
-    end,
-  },
-  {
-    'echasnovski/mini.icons',
-    version = false,
-    config = function()
-      require('mini.icons').setup()
-    end,
-  },
-  {
-    'echasnovski/mini.pairs',
-    version = false,
-    config = function()
-      require('mini.pairs').setup()
-    end,
-  },
-  {
-    'echasnovski/mini.surround',
-    version = false,
-    config = function()
-      require('mini.surround').setup()
-    end,
-  },
-  -- {
-  --   'echasnovski/mini.completion',
-  --   version = false,
-  --   config = function()
-  --     require('mini.completion').setup()
-  --   end,
-  -- },
+  'echasnovski/mini.nvim',
+  version = false,
+  config = function()
+    require('mini.ai').setup()
+    require('mini.icons').setup()
+    require('mini.pairs').setup()
+    require('mini.surround').setup()
+    require('mini.files').setup()
+  end,
 }
