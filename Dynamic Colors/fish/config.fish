b3dc3ca46bcac14c5313e7a33aa934b5a9bb0916
helixoid <ayushrajmth91@gmail.com>
2025-01-14 12:01:09 +0530

    Added Packages to make a install script later!!

--- Dynamic Colors/fish/config.fish
+++ Dynamic Colors/fish/config.fish
@@ -1,14 +1,12 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-fastfetch
+export TERMINAL=ghostty
 starship init fish | source
 atuin init fish | source
 alias v="nvim"
-alias hx="helix"
 alias prop="hyprctl clients | grep -i 'class\|title\|xwayland'"
-alias ls='lsd'
-alias la='lsd -a'
-alias lla='lsd -la'
-alias lt='lsd -la --tree'
-export TERM=kitty
+alias ls='eza'
+alias la='eza -a'
+alias lla='eza -la'
+alias lt='eza -la --tree'
 end
