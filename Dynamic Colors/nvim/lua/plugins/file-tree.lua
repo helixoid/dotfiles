6031f65da64ff9b76c47e43de097e3b852285c70
helixoid <ayushrajmth91@gmail.com>
2025-01-16 14:39:44 +0530

    Added neotree and border to completions!!

---
+++ Dynamic Colors/nvim/lua/plugins/file-tree.lua
@@ -0,0 +1,21 @@
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  branch = 'v3.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  cmd = 'Neotree',
+  keys = {
+    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+  },
+  opts = {
+    filesystem = {
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+    },
+  },
+}
