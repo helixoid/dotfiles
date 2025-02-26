785dfa6d71b2a5d39f5a7b125f09ba607e12d536
helixoid <ayushrajmth91@gmail.com>
2025-02-26 11:43:50 +0530

    NVIM Improved

--- Dynamic Colors/nvim/lua/plugins/formatter.lua
+++ Dynamic Colors/nvim/lua/plugins/formatter.lua
@@ -20,10 +20,11 @@ return {
     -- Define your formatters
     formatters_by_ft = {
       lua = { 'stylua' },
-      python = { 'isort', 'black' },
+      python = { 'ruff' },
       javascript = { 'prettierd', 'prettier', stop_after_first = true },
       yaml = { 'yamlfmt' },
       toml = { 'taplo' },
+      sh = { 'shfmt' },
       markdown = { 'prettierd', 'prettier', stop_after_first = true },
     },
     -- Set default options
